e719804f0dd4149098e871834bd7ed3f5e81cc5c
Anikalp Jaiswal <Anikalpjaiswal1@gmail.com>
2022-10-02 02:08:16 +0530

    Solution of Jump Game 2 has been added

---
+++ greedy/jumpgame2.cpp
@@ -0,0 +1,20 @@
+//Runtime: 28 ms
+//Memory Usage: 16.5 MB
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+vector<int> N;
+for(int i=0;i<10;i++)
+{
+    N.push_back(i);
+}
+        int len = N.size() - 1, curr = -1, next = 0, ans = 0;
+        for (int i = 0; next < len; i++) 
+        {
+            if (i > curr) ans++, curr = next;
+            next = max(next, N[i] + i);
+        }
+        cout<<ans<<endl;
+}
