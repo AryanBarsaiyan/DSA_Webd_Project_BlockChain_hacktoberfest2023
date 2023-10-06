b282e7cc4b375b396012d09f6e43ab9b37a670c3
Sidhant Mishra <69967446+rishi457@users.noreply.github.com>
2023-10-06 18:28:34 +0530

    Update BST_insert.cpp
    In this optimized version of the code, we have significantly reduced the time complexity for constructing a binary tree from its inorder and postorder traversals. Let's walk through the key changes and improvements:
    
    1. Hashmap for Inorder Indices:
       - We introduced an `unordered_map` named `indexMap` to store the indices of elements in the inorder traversal.
       - This `indexMap` allows us to look up the position of elements in O(1) time complexity, significantly improving the efficiency of our algorithm.
    
    2. Optimized Tree Construction:
       - The `Ctree` function now takes an additional parameter, `postIndex`, which is passed by reference.
       - We initialize `postIndex` to the last index of the postorder traversal array, allowing us to start building the tree from the rightmost elements.
       - As we construct the tree, we decrement `postIndex` to move backward in the postorder traversal array.
       - Using the `indexMap`, we efficiently locate the root element's position in the inorder traversal.
       - This optimized approach ensures that we construct the tree in linear time, resulting in a time complexity of O(n), where 'n' is the number of nodes in the tree.
    
    3. Main Function:
       - In the `main` function, we define the inorder and postorder traversal arrays and determine their size 'n'.
       - We call the `buildTree` function, which initializes the `indexMap` and constructs the binary tree using the optimized `Ctree` function.
       - After constructing the tree, we print the inorder traversal to verify its correctness.
    
    By implementing these changes, we have significantly improved the efficiency of our tree construction algorithm, reducing the time complexity from O(n^2) to O(n). This ensures faster execution for larger trees and is a more scalable solution.

--- BST Tree/BST_insert.cpp
+++ BST Tree/BST_insert.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 class node
 {
     public:
@@ -8,97 +9,53 @@ class node
         node *right;
         node(int x)
         {
-            key=x;
-            left=right=NULL;
+            key = x;
+            left = right = NULL;
         }
 };
-void printpost(node * root)
-{
-    if(root!=NULL)
-    {
-        printpost(root->left);
-        printpost(root->right);
-        cout<<root->key<<" ";
-    }
-}
-void printin(node *root)
-{
-    if(root!=NULL)
-    {
-        printin(root->left);
-        cout<<root->key<<" ";
-        printin(root->right);
-    }
-}
 
-node * Ctree(int in[],int po[],int is,int ie)
+unordered_map<int, int> indexMap;
+
+node* Ctree(int in[], int po[], int is, int ie, int& postIndex)
 {
-    if(is>ie)   return NULL;
-    static int index=ie;
-    node *root=new node(po[index--]);
-    int preindex=0;
-    for(int i=is;i<=ie;i++)
-    {
-        if(root->key==in[i])
-        {
-            preindex=i;
-            break;
-        }
-    }
-    root->right=Ctree(in,po,preindex+1,ie);
-    root->left=Ctree(in,po,is,preindex-1);
-    return root;
-}
-node * InsertRoot(node * root,int x)
-{
-    if(root==NULL)
-    {
-        root=new node(x);
-        return root;
-    }
-    node * cur=root;
-    node * parent=root;
-    while(cur!=NULL)
-    {
-        parent=cur;
-        if(cur->key==x)
-            return root;
-        else if(cur->key>x)
-            cur=cur->left;
-        else
-            cur=cur->right;
-    }
-    cur=new node(x);
-    if(parent->key<x)
-    parent->right=cur;
-    else
-    parent->left=cur;
+    if (is > ie) return NULL;
+
+    int rootValue = po[postIndex--];
+    node* root = new node(rootValue);
+    int inIndex = indexMap[rootValue];
+
+    root->right = Ctree(in, po, inIndex + 1, ie, postIndex);
+    root->left = Ctree(in, po, is, inIndex - 1, postIndex);
+
     return root;
 }
 
-node *insertRootRecursive(node *root,int x)
+node* buildTree(int in[], int po[], int n)
 {
-    if(root==NULL)
-        root=new node(x);
-    else if(root->key<x)
-        root->right=insertRootRecursive(root->right,x);
-    else
-        root->left=insertRootRecursive(root->left,x);
-    return root;
+    int postIndex = n - 1;
+    for (int i = 0; i < n; i++) {
+        indexMap[in[i]] = i;
+    }
 
+    return Ctree(in, po, 0, n - 1, postIndex);
+}
+
+void printInorder(node* root)
+{
+    if (root == NULL) return;
+    printInorder(root->left);
+    cout << root->key << " ";
+    printInorder(root->right);
 }
 
 int main()
 {
-    int in[]={5,10,12,15,18};
-    int post[]={5,12,18,15,10};
-    node * root=Ctree(in,post,0,4);
-    //cout<<endl;
-   // printpost(root);
-   //root=insertRootRecursive(root,20);
-   root=InsertRoot(root,20);
-   printin(root);
-   cout<<endl;
-   printpost(root);
-    
+    int in[] = {5, 10, 12, 15, 18};
+    int post[] = {5, 12, 18, 15, 10};
+    int n = sizeof(in) / sizeof(in[0]);
+
+    node* root = buildTree(in, post, n);
+    printInorder(root);
+    cout << endl;
+    return 0;
 }
